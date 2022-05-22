95231732c63b116795fb4a8db2dd40761942007f
JUNGHOJONG <hojong1351@naver.com>
2022-05-23 01:24:34 +0900

    Initial commit

---
+++ src/test/java/study/querydsl/QuerydslApplicationTests.java
@@ -0,0 +1,35 @@
+package study.querydsl;
+
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import study.querydsl.entity.Hello;
+import study.querydsl.entity.QHello;
+
+import javax.persistence.EntityManager;
+import javax.transaction.Transactional;
+
+@Transactional // 이거 없으면 롤백 안됨
+@SpringBootTest
+class QuerydslApplicationTests {
+
+	@Autowired
+	EntityManager em;
+
+	@Test
+	void contextLoads() {
+		Hello hello = new Hello();
+		em.persist(hello);
+
+		JPAQueryFactory query = new JPAQueryFactory(em);
+		final QHello qHello = QHello.hello; // Querydsl Q타입 동작 확인
+
+		Hello result = query.selectFrom(qHello).fetchOne();
+
+		Assertions.assertThat(result).isEqualTo(hello);
+		Assertions.assertThat(result.getId()).isEqualTo(hello.getId());
+	}
+
+}
